85f0a58d53f69475f1ff289c218992d7b4c896a3
Ubuntu <ubuntu@ip-172-31-16-199.us-east-2.compute.internal>
2021-11-28 07:10:36 +0000

    Init lens backend

---
+++ lens_backend/indexer.py
@@ -0,0 +1,143 @@
+import os
+import pysolr
+import requests
+import json
+import pandas as pd
+import pickle5 as pickle
+import glob
+
+# https://tecadmin.net/install-apache-solr-on-ubuntu/
+
+
+CORE_NAME = "IRF_21"
+AWS_IP = "localhost"
+
+
+# [CAUTION] :: Run this script once, i.e. during core creation
+
+
+def delete_core(core=CORE_NAME):
+    print(os.system('sudo su - solr -c "/opt/solr/bin/solr delete -c {core}"'.format(core=core)))
+
+
+def create_core(core=CORE_NAME):
+    print(os.system(
+        'sudo su - solr -c "/opt/solr/bin/solr create -c {core} -n data_driven_schema_configs"'.format(
+            core=core)))
+
+
+# collection
+
+collection = [{
+    "id": 1,
+    "first_name": "Chickie",
+    "last_name": "Proven",
+    "email": "cproven0@alexa.com",
+    "age": 77,
+    "pincodes": [2121212, 3232323]
+}, {
+    "id": 2,
+    "first_name": "Dex",
+    "last_name": "Bofield",
+    "email": "dbofield1@about.com",
+    "age": 88,
+    "pincodes": [2121212, 3232323]
+}, {
+    "id": 3,
+    "first_name": "Saba",
+    "last_name": "Ace",
+    "email": "sace2@craigslist.org",
+    "age": 55,
+    "pincodes": [2121212, 3232323]
+}]
+
+class Indexer:
+    def __init__(self):
+        self.solr_url = f'http://{AWS_IP}:8983/solr/'
+        self.connection = pysolr.Solr(self.solr_url + CORE_NAME, always_commit=True, timeout=5000000)
+
+    def do_initial_setup(self):
+        delete_core()
+        create_core()
+
+    def create_documents(self, docs):
+        print("OK")
+        print(self.connection.add(docs))
+
+    def connection_object(self):
+        return self.connection
+
+    def add_fields(self):
+        '''
+        Define all the fields that are to be indexed in the core. Refer to the project doc for more details
+        :return:
+        '''
+        print("IN add_fields")
+        data = {
+            "add-field": [
+                #{"name": "id","type": "string","multiValued": False},
+                {"name": "country","type": "string","multiValued": False},
+                {"name": "tweet_lang","type": "string","multiValued": False},
+                {"name": "tweet_text","type": "text_general","multiValued": False},
+                {"name": "text_en","type": "text_en","multiValued": False},
+                {"name": "text_hi", "type": "text_hi", "multiValued": False},
+                {"name": "text_es", "type": "text_es", "multiValued": False},
+                {"name": "tweet_date", "type": "pdate", "multiValued": False},
+                {"name": "verified", "type": "boolean", "multiValued": False},
+                {"name": "poi_id", "type": "plong", "multiValued": False},
+                {"name": "poi_name", "type": "string", "multiValued": False},
+                {"name": "replied_to_tweet_id", "type": "plong", "multiValued": False},
+                {"name": "replied_to_user_id", "type": "plong", "multiValued": False},
+                {"name": "reply_text", "type": "text_general", "multiValued": False},
+                {"name": "hashtags", "type": "strings", "multiValued": True},
+                {"name": "mentions", "type": "strings", "multiValued": True},
+                {"name": "tweet_urls", "type": "strings", "multiValued": True},
+                {"name": "tweet_emoticons", "type": "strings", "multiValued": True},
+                {"name": "geolocation", "type": "strings", "multiValued": True}
+            ]
+        }
+
+        print(requests.post(self.solr_url + CORE_NAME + "/schema", json=data).json())
+
+        #raise NotImplementedError
+
+    def delete_fields(self):
+        data = {
+            "delete-field": [
+                {
+                    "name": "id"
+                }
+            ]
+        }
+
+        print(requests.post(self.solr_url + CORE_NAME + "/schema", json=data).json())
+
+def add_data(indexer):
+    #with open("train.json") as json_file:
+    #    data = json.load(json_file)
+
+    root_dir = '/home/ubuntu/IRF_Project_21/project4/project1_data/'
+    dir_names = glob.glob(root_dir + "/*")
+
+    for dir_name in dir_names:
+        file_names = glob.glob(dir_name + "/*")
+        for file_name in file_names:
+
+            if '.csv' in file_name:
+                continue
+                
+            with open(file_name, 'rb') as f:
+                data = pickle.load(f)
+
+            json_list = json.loads(json.dumps(list(data.T.to_dict().values())))
+
+            print(len(json_list))
+            indexer.create_documents(json_list)
+
+if __name__ == "__main__":
+    i = Indexer()
+    #i.do_initial_setup()
+    #i.add_fields()
+    add_data(i)
+    #i.delete_fields()
+   # i.create_documents(collection[0])
---
+++ lens_backend/solr_search.py
@@ -0,0 +1,200 @@
+import indexer
+import json
+import emoji
+import pickle
+import pysolr
+
+import simplejson
+import pprint
+
+
+import urllib.request as urllib2
+from urllib.parse import quote
+
+import pandas as pd
+
+import collections
+from nltk.stem import PorterStemmer
+import re
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
+
+import re
+from urllib.parse import urlparse
+from urllib.parse import urlunparse
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import gensim
+from gensim.summarization.summarizer import summarize
+
+import flask
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+ind = indexer.Indexer()
+
+def save_file(data, filename):
+    df = pd.DataFrame(data)
+    df.to_pickle("data/all_data/" + filename)
+
+def remove_emoji(text):
+    return emoji.get_emoji_regexp().sub(u'', text)
+
+def solr_search_query(connection, query, rows=0):
+    results = connection.search(q=query, rows=rows)
+    print("Saw {0} result(s).".format(len(results)))
+    return results
+
+def tokenizer(text):
+    """ Implement logic to pre-process & tokenize document text.
+        Write the code in such a way that it can be re-used for processing the user's query.
+        To be implemented."""
+
+    # print("Original ---->>>>  ", text)
+    stop_words = set(stopwords.words('english'))
+
+    text = text.lower()
+    text = text.replace("'", "")
+    text = text.replace("’", "")
+    text = text.replace("#", "%23")
+    #text = re.sub(r'[-]+', ' ', text)
+    #text = re.sub(r'[^A-Za-z0-9 ]+', ' ', text)
+    text = re.sub('[,]', ' ', text)
+    text = re.sub(' +', ' ', text)
+
+
+    tokenized_text = text.split()
+
+    #print(text)
+    #print(tokenized_text)
+
+    ps = PorterStemmer()
+    tokenized_text = [word for word in tokenized_text if word not in stop_words]
+    #tokenized_text = [ps.stem(word) for word in tokenized_text]
+
+    # print(tokenized_text)
+
+    return tokenized_text
+
+def get_from_solr(core_name, text):
+    host = "localhost"
+    port = "8983"
+    collection = core_name
+    qt = "select"
+    url = "http://" + host + ":" + port + "/solr/" + collection + "/" + qt + "?"
+
+    #text = text[:-1]
+    query_string = "text_en%3A" + text + "text_hi%3A" + text + "text_es%3A" + text
+
+   # query_string = "\"" + query_string + "\"~3"
+    print("Query------>>>>>  ", query_string)
+    q = "defType=edismax&df=text_en&df=text_hi&df=text_es&q=" + query_string
+    q = "defType=edismax&q=" + query_string
+    q += "&qf=hashtags%5E1.5%20text_en%5E1.5%20text_hi%5E1.5%20text_es%5E1.5"
+
+    extra_percent = '%2C%20'
+    result_fields = "id" + extra_percent + 'poi_name' + extra_percent + 'tweet_text'  + \
+                    extra_percent + 'hashtags' + extra_percent + 'tweet_date' + extra_percent + 'country' + extra_percent + 'score'
+
+    fl = "q.op=OR&fl=" + result_fields
+    fq = "fq="
+    rows = "rows=200000"
+    wt = "wt=json"
+    # wt        = "wt=python"
+    params = [q, fl, fq, wt, rows]
+    p = "&".join(params)
+    actual_url = url + (p)
+
+    print(actual_url)
+    #actual_url = 'http://3.134.191.90:8983/solr/IRF_21/select?fl=id%2C%20poi_name%2C%20score&q.op=OR&q=text_en%3AModi%20and%20India'
+    connection = urllib2.urlopen(actual_url)
+
+    if wt == "wt=json":
+        response = simplejson.load(connection)
+    else:
+        response = eval(connection.read())
+
+    print("Number of hits: " + str(response['response']['numFound']))
+
+    print(type(response))
+
+    dict_result = []
+
+    cnt =0
+    for doc in response['response']['docs']:
+        if 'poi_name' in doc.keys():
+            #print(doc['id'])
+            dict_result.append(doc)
+            cnt += 1
+            #if cnt == 10:
+                #break
+
+    analyzer = SentimentIntensityAnalyzer()
+    lens_doc = []
+    for doc in dict_result:
+        vs = analyzer.polarity_scores(doc['tweet_text'])
+       # print( vs['pos'])
+        doc['sentiment'] = vs['pos']
+
+        if len(doc['tweet_text']) < 500000:
+            short_summary = doc['tweet_text']
+        else:
+            short_summary = summarize(doc['tweet_text'])
+
+        doc['topics'] = short_summary
+
+        doc['top_pos_reply'] = 'Top Positive reply. need to be implemented'
+        doc['top_neg_reply'] = 'Top Negative reply. need to be implemented'
+
+        lens_doc.append(doc)
+        #print(dict_result[0])
+
+    print(lens_doc[0].keys())
+
+    return lens_doc
+
+
+
+def search_query(query, solr_core):
+    connection = solr_core.connection
+    lang = 'Mexico'
+    search_query = 'country:' + lang
+    core_name = 'IRF_21'
+
+
+    #print("Collecting data  ", line[0], line[1::])
+    search_query = ""
+
+    tokenized_text = tokenizer(query)
+    print(tokenized_text)
+    #continue
+    for word in tokenized_text:
+        search_query += word + "%20"
+    print("Search query==", search_query)
+    #search_query = re.sub(r'[^\x00-\x7F]+',' ', search_query)
+    print("query id   core name and query  _______>>>>>>>>>    ", core_name, search_query)
+    lens_doc = get_from_solr(core_name, (search_query))
+
+    return lens_doc
+
+@app.route("/execute_query", methods=['POST'])
+def execute_query(query):
+    """ This function handles the POST request to your endpoint.
+        Do NOT change it."""
+    start_time = time.time()
+
+    # Search document in solr with query
+    lens_doc = search_query(query, ind)
+
+    response = {
+        "Response": lens_doc,
+        "time_taken": str(time.time() - start_time)
+    }
+    return flask.jsonify(response)
+
+if __name__ == "__main__":
+    search_query("Modi and India", ind)
+    #app.run(host="0.0.0.0", port=9999)
