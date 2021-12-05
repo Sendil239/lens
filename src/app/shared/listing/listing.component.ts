0b0f854ba9926f59557e00ba06a209795e5c17f0
Subramani <sramadas@buffalo.edu>
2021-12-05 02:37:37 -0500

    paginator initial implm

--- src/app/shared/listing/listing.component.ts
+++ src/app/shared/listing/listing.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ITwitterData } from '../interfaces/twitter_data.interface';
+import {PageEvent} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-listing',
@@ -7,14 +8,27 @@ import { ITwitterData } from '../interfaces/twitter_data.interface';
   styleUrls: ['./listing.component.scss']
 })
 export class ListingComponent implements OnInit {
-  
+  length: number;
+  pageSize: number;
+  pageSizeOptions: number[];
+  pageEvent: PageEvent;
+
   constructor() {
+    this.length = 100;
+    this.pageSize = 10;
+    this.pageSizeOptions = [5, 10, 25, 100];
   }
 
   @Input('tweetsList')
   tweetsList: ITwitterData[];
 
   ngOnInit(): void {
+  }
+
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
   }
 
 }
