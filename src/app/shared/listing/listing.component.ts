605ae6ea0d970ea363419f6049c6688a204c4d97
Subramani <sramadas@buffalo.edu>
2021-12-07 15:30:19 -0500

    tweet date changes

--- src/app/shared/listing/listing.component.ts
+++ src/app/shared/listing/listing.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ITwitterData } from '../interfaces/twitter_data.interface';
 import {PageEvent} from '@angular/material/paginator';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-listing',
@@ -12,6 +13,8 @@ export class ListingComponent implements OnInit {
   pageSize: number;
   pageSizeOptions: number[];
   pageEvent: PageEvent;
+  tweetsList: ITwitterData[];
+
 
   @Output() pageUpdated = new EventEmitter();
 
@@ -21,7 +24,12 @@ export class ListingComponent implements OnInit {
   }
 
   @Input('tweetsList')
-  tweetsList: ITwitterData[];
+  set setTweetsList(data: ITwitterData[]){
+    this.tweetsList = data;
+    this.tweetsList.forEach((x)=> {
+      x.link = x.tweet_text.substring(x.tweet_text.indexOf("https://"), x.tweet_text.lastIndexOf(""));
+    });
+  }
 
   @Input('totalTweetsCount')
   length: number;
@@ -32,6 +40,38 @@ export class ListingComponent implements OnInit {
 
   paginationUpdated(data: any) {
     this.pageUpdated.emit(data.pageIndex + 1);
-}
+  }
+
+  // format date in typescript
+  getFormatedDate(date: Date, format: string) {
+    const datePipe = new DatePipe('en-US');
+    return datePipe.transform(date, format);
+  }
+
+  convertToLocalDate(responseDate: any) {
+    try {
+        if (responseDate != null) {
+            if (typeof (responseDate) === 'string') {
+                if (String(responseDate.indexOf('T') >= 0)) {
+                    responseDate = responseDate.split('T')[0];
+                }
+                if (String(responseDate.indexOf('+') >= 0)) {
+                    responseDate = responseDate.split('+')[0];
+                }
+            }
+
+            responseDate = new Date(responseDate);
+            const newDate = new Date(responseDate.getFullYear(), responseDate.getMonth(), responseDate.getDate(), 0, 0, 0);
+            const userTimezoneOffset = newDate.getTimezoneOffset() * 60000;
+
+            const finalDate: Date = new Date(newDate.getTime() - userTimezoneOffset);
+            return finalDate ? finalDate : null;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        return responseDate;
+    }
+  }
 
 }
